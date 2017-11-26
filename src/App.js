e6a5b7ac89c1a095b3054f658d3937593d7afaae
MaltbyRL <RLMaltby@gmail.com>
2017-11-26 13:47:54 -0800

    COMMITING

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Intro />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render(){
+    return(
+      <div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+      </div>
+    );
+  }
+}
+
+class Intro extends Component {
+  render(){
+    return(
+      <div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,5 +37,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;
