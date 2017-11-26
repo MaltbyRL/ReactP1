276aa8ce2f3f68ba969d33c69a26e9c3a7e7ebaf
MaltbyRL <RLMaltby@gmail.com>
2017-11-26 14:50:58 -0800

    Added components ' '

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js'
+import Intro from './Intro.js'
+
 
 class App extends Component {
   render() {
@@ -13,30 +16,6 @@ class App extends Component {
   }
 }
 
-class Header extends Component {
-  render(){
-    return(
-      <div>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Intro extends Component {
-  render(){
-    return(
-      <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
 
 
